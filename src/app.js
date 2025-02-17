e613c7e29b478bfe103b5ab5efa86eadb7344210
Mayank Sharma <sharma.mayank3011@gmail.com>
2025-02-18 01:16:58 +0530

    - Added schema validations - Added API validations - Added Data sanitization

--- src/app.js
+++ src/app.js
@@ -9,14 +9,57 @@ const app = express();
 app.use(express.json());
 
 app.post('/signup', async (req, res) =>{
-  const { firstName, lastName, email, password } = req.body;
-  const user = new User({ firstName, lastName, email, password });
+  const { firstName, lastName, email, password, about, skills, gender, age, photoUrl } = req.body;
+  const user = new User({ firstName, lastName, email, password, about, skills, gender, age, photoUrl });
+  res.setHeader('Content-Type', 'application/json');
 
+  
   try{
+    if(req.body.skills.length > 10){
+      throw new Error("Skills must be at most 10.");
+    }
     await user.save();
     res.send("User saved successfully");
   }catch(err){
-    res.status(400).send("User Is Not Saved!!"+ err.message);
+    res.status(400).send("Error: "+ err);
+    
+  }
+})
+
+app.get('/users', async (req, res) =>{
+
+  const email = req.query.email;
+
+  let users = await User.find({ email: email });
+
+  if(users.length == 0){
+    users = await User.find({});
+  }
+
+  try{
+    res.send(users);
+  }catch(err){
+    res.status(400).send("Something Went Wrong."+ err.message);
+    
+  }
+})
+
+app.patch('/user/:email', async (req, res) =>{
+
+  const email = req.params?.email;
+
+  if(req.body.skills.length > 10){
+    throw new Error("Skills must be at most 10.");
+  }
+
+  const userData = req.body;
+
+  let user = await User.findOneAndUpdate({ email: email }, userData, {returnDocument: after, runValidators: true});
+
+  try{
+    res.send(user);
+  }catch(err){
+    res.status(400).send("Something Went Wrong."+ err.message);
     
   }
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,67 @@
 const mongoose = require('mongoose');
+var validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  firstName: String, // String is shorthand for {type: String}
-  lastName: String,
-  email: String,
-  password: String,
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50
+  },
+  lastName: {
+    type: String,
+    minLength: 4,
+    maxLength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    lowercase: true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Enter Correct Email!")
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error('Password is not strong!');
+      }
+    }
+  },
+  age: {
+    type: Number
+  }, 
+  gender: {
+    type: String,
+    enum: {
+      values: ["male", "female", "other"],
+      message: `{VALUE} is not a valid gender type`,
+    },
+  },
+  photoUrl: {
+    type: String,
+    default: "https://geographyandyou.com/images/user-profile.png",
+    validate(value) {
+      if (!validator.isURL(value)) {
+        throw new Error("Invalid Photo URL: " + value);
+      }
+    },
+  },
+  about: {
+    type: String,
+    default: "this is default about.",
+  },
+  skills: {
+    type: [String],
+  }
+}, {
+  timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
